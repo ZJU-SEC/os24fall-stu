2c4a5d5dc08fbec01c6b6597141afcb41c28a33f
Neil <neilalfred93@gmail.com>
2024-12-10 15:19:26 +0800

    fix lab6 SEEK_* macros

--- src/lab6/user/unistd.h
+++ src/lab6/user/unistd.h
@@ -9,9 +9,9 @@
 #define O_WRONLY    0x0002
 #define O_RDWR      0x0003
 
-#define SEEK_SET    0x0001
-#define SEEK_CUR    0x0002
-#define SEEK_END    0x0003
+#define SEEK_SET    0x0000
+#define SEEK_CUR    0x0001
+#define SEEK_END    0x0002
 
 int open(char *filename, int flags);
 int write(int fd, const void *buf, uint64_t count);
